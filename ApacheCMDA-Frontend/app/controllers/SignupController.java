7ddb92a6024dd1eed8f55f4df45824273cde1f19
Kstain <kstainwwy@gmail.com>
2015-11-06 02:35:19 -0800

    signup init

---
+++ ApacheCMDA-Frontend/app/controllers/SignupController.java
@@ -0,0 +1,48 @@
+package controllers;
+
+/**
+ * Created by stain on 11/6/2015.
+ */
+
+import java.util.Iterator;
+import java.util.Map.Entry;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.gson.JsonObject;
+import play.api.mvc.Request;
+import play.libs.Json;
+import play.mvc.*;
+import views.html.*;
+import play.data.*;
+import util.APICall;
+import util.Constants;
+import util.APICall.ResponseType;
+import play.Logger;
+import models.User;
+
+public class SignupController  extends Controller {
+    final static Form<User> f_user = Form.form(User.class);
+
+    public static Result signUp() {
+        return ok(signup.render());
+    }
+
+    public static Result register() {
+        Form<User> form = f_user.bindFromRequest();
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode jnode = mapper.createObjectNode();
+        jnode.put("email", form.field("email").value());
+        jnode.put("password", form.field("password").value());
+        JsonNode usernode = User.register(jnode);
+        if (usernode == null || usernode.has("error")) {
+            Logger.debug("Register Failed!");
+            return redirect(routes.SignupController.signUp());
+        }
+        Logger.debug("New user created");
+        return redirect(routes.Application.login());
+    }
+
+
+
+}
