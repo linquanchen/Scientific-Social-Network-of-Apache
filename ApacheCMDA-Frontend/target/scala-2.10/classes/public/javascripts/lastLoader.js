e56e9c9fb3c40bfe75bbef071888b6ff9c6cd12f
GavinCode <gmy.gavin@gmail.com>
2015-11-21 12:36:18 -0800

    clean github

--- ApacheCMDA-Frontend/target/scala-2.10/classes/public/javascripts/lastLoader.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() 
-    { 
-        $("#myTable").tablesorter(); 
-        $("#myTable2").tablesorter(); 
-        $("#myTable3").tablesorter();
-        $("#csTable").tablesorter(); 
-    } 
-); 
