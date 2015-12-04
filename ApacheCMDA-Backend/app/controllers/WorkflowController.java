8195fd9f5ba88f4c103d9937ed1f44b522894a1c
baishi <baishi@baishideMacBook-Pro.local>
2015-12-03 19:38:09 -0800

    Add Comment

--- ApacheCMDA-Backend/app/controllers/WorkflowController.java
+++ ApacheCMDA-Backend/app/controllers/WorkflowController.java
@@ -18,19 +18,15 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import models.*;
-
 import play.mvc.Controller;
 import play.mvc.Result;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
-import javax.persistence.PersistenceException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -43,13 +39,16 @@ public class WorkflowController extends Controller {
     private final WorkflowRepository workflowRepository;
     private final UserRepository userRepository;
     private final GroupUsersRepository groupUsersRepository;
+    private final CommentRepository commentRepository;
 
     @Inject
     public WorkflowController(final WorkflowRepository workflowRepository,
-                              UserRepository userRepository, GroupUsersRepository groupUsersRepository) {
+                              UserRepository userRepository, GroupUsersRepository groupUsersRepository,
+                              CommentRepository commentRepository) {
         this.workflowRepository = workflowRepository;
         this.userRepository = userRepository;
         this.groupUsersRepository = groupUsersRepository;
+        this.commentRepository = commentRepository;
     }
 
     public Result post() {
@@ -169,5 +168,42 @@ public class WorkflowController extends Controller {
         }
 
         return ok(result);
+    }
+
+    public Result addComment(){
+        try{
+            JsonNode json = request().body().asJson();
+            if(json==null){
+                System.out.println("Comment not created, expecting Json data");
+                return badRequest("Comment not created, expecting Json data");
+            }
+
+            long userId = json.path("userID").asLong();
+            long timestamp = json.path("timestamp").asLong();
+            long workflowId = json.path("workflowID").asLong();
+            String content = json.path("Content").asText();
+
+            User user = userRepository.findOne(userId);
+            if(user==null){
+                System.out.println("Cannot find user with given user id");
+                return badRequest("Cannot find user with given user id");
+            }
+            Workflow workflow = workflowRepository.findOne(workflowId);
+            if(workflow==null){
+                System.out.println("Cannot find workflow with given workflow id");
+                return badRequest("Cannot find workflow with given workflow id");
+            }
+            Comment comment = new Comment(user, timestamp, content);
+            commentRepository.save(comment);
+            List<Comment> list = workflow.getComments();
+            list.add(comment);
+            workflow.setComments(list);
+
+            workflowRepository.save(workflow);
+            return ok("Comment add successfully");
+        } catch (Exception e){
+            e.printStackTrace();
+            return badRequest("Failed to add comment!");
+        }
     }
 }
